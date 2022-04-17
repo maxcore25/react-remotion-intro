3032ffcf5fda32d431b117d33e8c3e6f107b1970
maxcore25 <mkorobov2002@mail.ru>
2022-04-17 10:12:12 +0300

    Repo Initialized

---
+++ src/Composition.tsx
@@ -0,0 +1,15 @@
+export const MyComposition = () => {
+	return (
+		<div
+			style={{
+				flex: 1,
+				textAlign: 'center',
+				fontSize: '7em',
+				background: '#333',
+				color: 'magenta',
+			}}
+		>
+			Hello World
+		</div>
+	);
+};
