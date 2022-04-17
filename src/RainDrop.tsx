e9f866b7775a1b8bfcceb7d5663853b88ad1a5da
maxcore25 <mkorobov2002@mail.ru>
2022-04-17 10:36:56 +0300

    Rain Drop Created

---
+++ src/RainDrop.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const RainDrop: React.FC = () => {
+	return (
+		<div
+			style={{
+				width: 100,
+				height: 100,
+				background: 'lightblue',
+				borderRadius: '50%',
+			}}
+		/>
+	);
+};
+
+export default RainDrop;
